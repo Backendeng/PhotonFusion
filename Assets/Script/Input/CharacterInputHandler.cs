5ba68a14b608c69ee040e8afd3379bedfb50a544
Backendeng <backendeng0417@gmail.com>
2024-01-20 01:03:19 -0800

    adding movment and camera & adding gravity in preference

--- Assets/Script/Input/CharacterInputHandler.cs
+++ Assets/Script/Input/CharacterInputHandler.cs
@@ -5,26 +5,52 @@ using UnityEngine;
 public class CharacterInputHandler : MonoBehaviour
 {
     Vector2 moveInputVector = Vector2.zero;
+    Vector2 viewInputVector = Vector2.zero;
+    bool isJumpButtonPressed = false;
+
+    //other components
+    CharacterMovementHandler characterMovementHandler;
+
+    private void Awake()
+    {
+        characterMovementHandler = GetComponent<CharacterMovementHandler>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // View input
+        viewInputVector.x = Input.GetAxis("Mouse X");
+        viewInputVector.y = Input.GetAxis("Mouse Y")  * - 1;
+
+        characterMovementHandler.SetViewInputVector(viewInputVector);
+
         // Move input
         moveInputVector.x = Input.GetAxis("Horizontal");
         moveInputVector.y = Input.GetAxis("Vertical");
+
+        isJumpButtonPressed = Input.GetButtonDown("Jump");
     }
 
     public NetworkInputData GetNetworkInput()
     {
         NetworkInputData networkInputData = new NetworkInputData();
 
+        // view data
+        networkInputData.rotationInput = viewInputVector.x;
+
+        // Move data
         networkInputData.movementInput = moveInputVector;
+
+        // Jump data
+        networkInputData.isJumpPressed = isJumpButtonPressed;
 
         return networkInputData;
     }
