d5b9be9eaacc666f2a57d8141d7f6ddc291cb1c7
Backendeng <backendeng0417@gmail.com>
2024-01-19 20:11:02 -0800

    spawner and network runnner

---
+++ Assets/Script/Utils/Utils.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class Utils : MonoBehaviour 
+{
+    public static Vector3 GetRandomSpawnPoint()
+    {
+        return new Vector3(Random.Range(-20, 20), 4, Random.Range(-20, 20));
+    }
+   
+}
