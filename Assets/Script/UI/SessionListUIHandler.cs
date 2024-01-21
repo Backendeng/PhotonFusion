cccb513ea8bf27b5b268c07b8161c55d476bc739
Backendeng <backendeng0417@gmail.com>
2024-01-20 22:58:34 -0800

    creating sessin list ui

---
+++ Assets/Script/UI/SessionListUIHandler.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Fusion;
+
+public class SessionListUIHandler : MonoBehaviour
+{
+    public TextMeshProUGUI statusText;
+    public GameObject sessionItemListPrefab;
+    public VerticalLayoutGroup verticalLayoutGroup;
+
+    public void ClearList()
+    {
+        //Delete all children of the vertical layout group
+        foreach (Transform child in verticalLayoutGroup.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        //Hide the status message
+        statusText.gameObject.SetActive(false);
+    }
+
+    public void AddtToList(SessionInfo sessionInfo)
+    {
+        // Add a new item to the list
+        SessionInfoListUIItem addedSessionInfoListUIItem = Instantiate(sessionItemListPrefab, verticalLayoutGroup.transform).GetComponent<SessionInfoListUIItem>();
+
+        addedSessionInfoListUIItem.SetInformation(sessionInfo);
+
+        // Hook up events
+        addedSessionInfoListUIItem.onJoinSession += AddedSessionInfoListUIItem_OnJoinSession;
+    }
+
+    private void AddedSessionInfoListUIItem_OnJoinSession(SessionInfo obj)
+    {
+
+    }
+
+    public void OnNoSessionFound()
+    {
+        statusText.text = "No game session found";
+        statusText.gameObject.SetActive(true);
+    }
+
+    public void OnLookingForGameSessions()
+    {
+        statusText.text = "Looking for game sessions";
+        statusText.gameObject.SetActive(true);
+    }
+}
+
